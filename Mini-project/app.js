322ee7355c7185a0df953aec6edac03d94376620
https://github.com/Hasnain-KH <webdevhasnain@gmail.com>
2026-05-24 19:08:03 +0500

    Added One more Feature

--- Mini-project/app.js
+++ Mini-project/app.js
@@ -1,8 +1,17 @@
 gsap.to("img", {
-    y: -380,
+    y: -400,
     delay: 1,
-    duration: 0.6,
+    duration: 0.7,
     repeat: -1,
     yoyo: true,
     rotate: -60,
+})
+
+gsap.to(".backgroundimg", {
+    y: 20,
+    duration: 0.7,
+    delay: 1,
+    repeat: -1,
+    yoyo: true,
+    trasition: 2
 })
